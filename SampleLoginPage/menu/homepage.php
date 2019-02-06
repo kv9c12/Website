17175502067dd3a9069451ed120ab79fd705c679
kada varun <varun9c12@gmail.com>
2019-02-06 15:49:34 +0530

    Homepage modified

--- SampleLoginPage/menu/homepage.php
+++ SampleLoginPage/menu/homepage.php
@@ -2,19 +2,28 @@
 <html>
 <head>
 	<title>Dashboard</title>
-<link rel="stylesheet" href="../css/webpage.css">
+<link rel="stylesheet" type="text/css" href="../css/webpage.css">
 </head>
 <body style="background-image: url('../images/back.jpg');background-size: cover;">	
-	<img id="logo" src="../images/logo.jpg">
-	<div id="title"><center style="margin-left: 10%"> KGS Courier System </center></div>
+	<img id="logo" src="../images/logo.png">
+	<div id="title"><center style="margin-left: 10%;"> KGS Courier Services </center></div>
+	<div id="board">
+		<table>
+			<tr>
+				<th id="th1"><img src="../images/accimg.jpg"></th>
+				<th id="th2">hello</th>
+				<th id="th3">namaste</th>
+			</tr>
+		</table>
+	</div>
 	<div id="menu">
 		<a href="homepage.php"><li style="margin-top: 10%;"><p>Dashboard</p></li></a>
-		<a href="shpctrl.php"><li><p>Shopping Central</p></li></a>
-		<a href="bskt.php"><li><p>Basket</p></li></a>
-		<a href="pror.php"><li><p>Purchase Orders</p></li></a>
-		<a href="bills.php"><li><p>Bills</p></li></a>
-		<a href="prhis.php"><li><p>Purchase History</p></li></a>
-		<a href="acc.php"><li><p>Manage Account</p></li></a>
+		<a href="order.php"><li><p>Place Order</p></li></a>
+		<a href="track.php"><li><p>Track</p></li></a>
+		<a href="request.php"><li><p>Requests</p></li></a>
+		<a href="payment.php"><li><p>Payment</p></li></a>
+		<a href="history.php"><li><p>Service History</p></li></a>
+		<a href="account.php"><li><p>Manage Account</p></li></a>
 	</div>
 </body>
 </html>
